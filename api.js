8841d17b7b233ba81d2b2e65708f5570c6281ad9
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-12-10 17:21:55 -0300

    feat: rotas

---
+++ api.js
@@ -0,0 +1,122 @@
+const API_URL = "https://68f53e90b16eb6f46836baec.mockapi.io/minhete/users";
+const API_URL_VEICULOS = "https://68f53e90b16eb6f46836baec.mockapi.io/minhete/veiculos";
+
+// Buscar usuários
+export const fetchUsers = async () => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data; 
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    return [];
+  }
+};
+
+// Buscar veículos
+export const fetchCars = async () => {
+  try {
+    const response = await fetch(API_URL_VEICULOS);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Erro ao buscar Veículos:", error);
+    return [];
+  }
+};
+
+// Cadastro de usuário
+export const registerUser = async (email, name, password) => {
+  console.log("Tentando cadastrar");
+
+  try {
+    const users = await fetchUsers();
+
+    const user = users.find((u) => u.email === email);
+
+    if (user) {
+      window.alert("Erro: Já existe um usuário com este email!");
+      return null;
+    }
+
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, name, password }),
+    });
+
+    console.log("Status da resposta:", response.status);
+    const data = await response.json();
+
+    window.alert("Usuário cadastrado com sucesso!");
+
+    return data;
+  } catch (error) {
+    console.error("Erro ao cadastrar:", error);
+    return null;
+  }
+};
+
+// Cadastro de veículo
+export const RegisterVeiculo = async (placa, dt_entrada) => {
+  console.log("Cadastrando veículo");
+
+  try {
+    const cars = await fetchCars();
+
+    const car = cars.find((c) => c.placa === placa);
+
+    if (car) {
+      window.alert("Erro: Já existe um carro com esta placa!");
+      return null;
+    }
+
+    const placaRegex = /^[A-Z]{3}-\d{4}$/;
+
+    if (!placaRegex.test(placa)) {
+      window.alert("Placa inválida! Digite no formato ABC-1234");
+      return null;
+    }
+
+    const response = await fetch(API_URL_VEICULOS, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ placa, dt_entrada }),
+    });
+
+    const data = await response.json();
+
+    window.alert("Veículo cadastrado com sucesso!");
+
+    console.log("Status da resposta:", response.status);
+    console.log("Resposta da API:", data);
+
+    return data;
+  } catch (error) {
+    console.error("Erro ao cadastrar veículo:", error);
+    return null;
+  }
+};
+
+// Saída de veículo
+export const saidaVeiculo = async (placa) => {
+  try {
+    const veiculos = await fetchCars();
+
+    const veiculo = veiculos.find((car) => car.placa === placa);
+
+    if (veiculo) {
+      const response = await fetch(`${API_URL_VEICULOS}/${veiculo.id}`, {
+        method: "DELETE",
+      });
+
+      window.alert("Veículo removido com sucesso!");
+      return response;
+    } else {
+      window.alert("Erro: Placa incorreta.");
+    }
+  } catch (error) {
+    console.error("Erro na saída:", error);
+    return null;
+  }
+};
--- stars-web/src/layouts/RegisterPage/index.jsx
+++ stars-web/src/layouts/RegisterPage/index.jsx
@@ -4,11 +4,13 @@ import back from "../../assets/images/back.png"
 import Button from "../../components/button/Button"
 import Input from "../../components/Input/input"
 import './styles.css'
+import { Link } from 'react-router-dom';
+
 export default function RegisterPage() {
     return (
         <div>
             <main>
-                <img src={back} alt="" className="img-back" />
+                <Link to={'/'} className="link"><img src={back} alt="" className="img-back" /></Link>
                 <section className="container-register">
                     <img src={planets} alt="" className="planets-img" />
                     <img src={logo} alt="" />
@@ -18,7 +20,7 @@ export default function RegisterPage() {
                         <Input placeholder={"Senha"} password={true} />
                     </form>
                     <footer className="footer-register">
-                        <Button label={"Criar"} color={"#FF92C2"} />
+                        <Button link={'/list'} label={"Criar"} color={"#FF92C2"} />
                     </footer>
                 </section>
             </main>
